9584461b1792c5683dd4a53f226bd3321ef015bc
Nikolay Iskorostensky <nikolay11.10.1990@gmail.com>
2024-05-02 00:33:36 +0300

    почав хедер айропорт

--- DZ_20_2/Airport.cpp
+++ DZ_20_2/Airport.cpp
@@ -1,1 +1,71 @@
 #include "Airport.h"
+
+Airport::Airport(const char* flight_number, const char* department_point, const char* point_of_importance, const char* Letak_brand, const char* how_many_places_there_are, const char* number_of_free_places, const char* passenger_list)
+{
+
+
+
+}
+
+Airport::Airport()
+{
+}
+
+Airport::~Airport()
+{
+}
+
+void Airport::set_flight_number(const char* flight_number)
+{
+}
+
+void Airport::set_department_point(const char* department_point)
+{
+}
+
+void Airport::set_point_of_importance(const char* point_of_importance)
+{
+}
+
+void Airport::set_passenger_list(const char* passenger_list)
+{
+}
+
+const char* Airport::get_flight_number()
+{
+    return nullptr;
+}
+
+const char* Airport::get_department_point()
+{
+    return nullptr;
+}
+
+const char* Airport::get_point_of_importance()
+{
+    return nullptr;
+}
+
+const char* Airport::get_Letak_brand()
+{
+    return nullptr;
+}
+
+const char* Airport::get_how_many_places_there_are()
+{
+    return nullptr;
+}
+
+const char* Airport::get_number_of_free_places()
+{
+    return nullptr;
+}
+
+const char* Airport::get_passenger_list()
+{
+    return nullptr;
+}
+
+void Airport::showInfoAirport()
+{
+}
